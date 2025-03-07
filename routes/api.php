8b0712fa4d997600dce9f149b35aabd68c6427bf
Elena Rocha <ele_r.v@hotmail.com>
2025-03-07 22:42:41 +0100

    Group routes

--- routes/api.php
+++ routes/api.php
@@ -12,19 +12,21 @@ Route::post('/login', [AuthController::class, 'login']);
 
 // Rutas protegidas
 Route::middleware('auth:sanctum')->group(function () {
-    
+
     // Perfil del usuario autenticado
-    Route::get('/perfil', [UserController::class, 'showProfile']); 
-    Route::put('/perfil', [UserController::class, 'updateProfile']); 
+    Route::prefix('perfil')->group(function () {
+        Route::get('/', [UserController::class, 'showProfile']);
+        Route::put('/', [UserController::class, 'updateProfile']);
+    });
 
     Route::post('/logout', [AuthController::class, 'logout']);
 
     // Rutas para CLIENTES
     Route::middleware('role:client')->group(function () {
-        Route::get('/mis-bonos', [PassController::class, 'getUserPasses']); 
+        Route::get('/mis-bonos', [PassController::class, 'getUserPasses']);
         Route::post('/adquirir-bono', [PassController::class, 'store']);
         Route::delete('/bonos/{id}', [PassController::class, 'destroy']);
-    
+
         Route::get('/mis-sesiones', [BookingController::class, 'getUserBookings']);
         Route::post('/reservar-sesion', [BookingController::class, 'store']);
         Route::delete('/sesiones/{id}', [BookingController::class, 'destroy']);
@@ -32,20 +34,29 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // Rutas para ADMINISTRADORES
     Route::middleware('role:admin')->group(function () {
-        Route::get('/clientes', [UserController::class, 'index']);
-        Route::post('/clientes', [UserController::class, 'store']);
-        Route::get('/clientes/{id}', [UserController::class, 'show']);
-        Route::put('/clientes/{id}', [UserController::class, 'update']);
-        Route::delete('/clientes/{id}', [UserController::class, 'destroy']);
-        Route::get('/clientes/{id}/bonos', [PassController::class, 'getUserPassesAdmin']);
-        Route::get('/clientes/{id}/sesiones', [BookingController::class, 'getUserBookingsAdmin']);
+        Route::prefix('clientes')->group(function () {
+            Route::get('/', [UserController::class, 'index']);
+            Route::post('/', [UserController::class, 'store']);
+        });
 
-        Route::get('/bonos', [PassController::class, 'index']);
-        Route::post('/bonos', [PassController::class, 'store']);
-        Route::delete('/bonos/{id}', [PassController::class, 'destroy']);
+        Route::prefix('clientes')->group(function () {
+            Route::get('/', [UserController::class, 'show']);
+            Route::put('/', [UserController::class, 'update']);
+            Route::delete('/', [UserController::class, 'destroy']);
+            Route::get('/bonos', [PassController::class, 'getUserPassesAdmin']);
+            Route::get('/sesiones', [BookingController::class, 'getUserBookingsAdmin']);
+        });
 
-        Route::get('/sesiones', [BookingController::class, 'index']);
-        Route::post('/sesiones', [BookingController::class, 'store']);
-        Route::delete('/sesiones/{id}', [BookingController::class, 'destroy']);
+        Route::prefix('bonos')->group(function () {
+            Route::get('/', [PassController::class, 'index']);
+            Route::post('/', [PassController::class, 'store']);
+            Route::delete('/{id}', [PassController::class, 'destroy']);
+        });
+
+        Route::prefix('sesiones')->group(function () {
+            Route::get('/', [BookingController::class, 'index']);
+            Route::post('/', [BookingController::class, 'store']);
+            Route::delete('/{id}', [BookingController::class, 'destroy']);
+        });
     });
 });
